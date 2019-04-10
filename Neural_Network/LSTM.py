149ed418a48db30582ab933922c00e700760c376
Chudleyj <chudleyjustin@gmail.com>
2019-04-09 22:47:26 -0400

    Final version

--- Neural_Network/LSTM.py
+++ Neural_Network/LSTM.py
@@ -3,7 +3,7 @@ import pandas as pd
 import talib
 import random
 import matplotlib.pyplot as plt
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import Dropout
@@ -21,16 +21,20 @@ dataset['Std_dev']= dataset['prices'].rolling(5).std()
 dataset['RSI'] = talib.RSI(dataset['prices'].values, timeperiod = 9)
 dataset['Price_Rise'] = np.where(dataset['prices'].shift(-1) > dataset['prices'], 1, 0)
 dataset = dataset.dropna()
-print("testttt")
+
 X = dataset.iloc[:,4:-1]
 y = dataset.iloc[:,-1]
 
-split = int(len(dataset)*0.8)
+split = int(len(dataset)*0.9) #train on x % of data
 X_train, X_test, y_train, y_test = X[:split], X[split:], y[:split], y[split:]
 
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
+
+output_data = MinMaxScaler(feature_range=(0,1))
+y_train = output_data.fit_transform(y_train.values.reshape(-1,1))
+y_test = output_data.fit_transform(y_test.values.reshape(-1,1))
 
 classifier = Sequential()
 
@@ -46,9 +50,10 @@ classifier.fit(X_train, y_train, batch_size = 10, epochs = 100)
 
 y_pred = classifier.predict(X_test)
 y_pred = (y_pred > 0.5)
+n_pred = output_data.inverse_transform(y_pred)
 
-dataset['y_pred'] = np.NaN
-dataset.iloc[(len(dataset) - len(y_pred)):,-1:] = y_pred
+dataset['n_pred'] = np.NaN
+dataset.iloc[(len(dataset) - len(n_pred)):,-1:] = n_pred
 trade_dataset = dataset.dropna()
 
 trade_dataset['Tomorrows Returns'] = 0.
@@ -56,7 +61,7 @@ trade_dataset['Tomorrows Returns'] = np.log(trade_dataset['prices']/trade_datase
 trade_dataset['Tomorrows Returns'] = trade_dataset['Tomorrows Returns'].shift(-1)
 
 trade_dataset['Strategy Returns'] = 0.
-trade_dataset['Strategy Returns'] = np.where(trade_dataset['y_pred'] == True, trade_dataset['Tomorrows Returns'], - trade_dataset['Tomorrows Returns'])
+trade_dataset['Strategy Returns'] = np.where(trade_dataset['n_pred'] == True, trade_dataset['Tomorrows Returns'], - trade_dataset['Tomorrows Returns'])
 
 trade_dataset['Cumulative Market Returns'] = np.cumsum(trade_dataset['Tomorrows Returns'])
 trade_dataset['Cumulative Strategy Returns'] = np.cumsum(trade_dataset['Strategy Returns'])
